560eb83049a488903461b473e6db394778a25e8c
crocodilelurker <swagatsahu556@gmail.com>
2025-09-17 22:11:42 +0530

    C01

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server is uptime on port " + PORT);
+})
